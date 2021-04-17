ae377a0521411e7577c9d53a88b453b83ec525c0
EiLunge <1124760813@qq.com>
2021-04-17 16:27:31 +0800

    第五次提交代码完成，修改3355为动态获取dev版本号

--- cloud-config-center3355/src/main/java/com/atguigu/springcloud/controller/ConfigCenterController.java
+++ cloud-config-center3355/src/main/java/com/atguigu/springcloud/controller/ConfigCenterController.java
@@ -1,12 +1,15 @@
 package com.atguigu.springcloud.controller;
 
+import jdk.nashorn.internal.ir.annotations.Reference;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@RefreshScope  //刷新功能
 public class ConfigCenterController {
     @Value("${config.info}")
     private String configInfo;
